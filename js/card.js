9a99ef5e1fe5950b6e650fae86cedbf8e259791f
Colton Boston <coltonbboston@gmail.com>
2018-10-26 21:37:18 -0500

    add switch statement to set card values

--- js/card.js
+++ js/card.js
@@ -1,7 +1,14 @@
-function Card(suit, rank, value){
-  this.suit = suit;
+function Card(rank, suit){
   this.rank = rank;
-  this.value = value;
+  this.suit = suit;
+  
+  switch (rank){
+    case 'Ace': this.value = 11; break;
+    case 'Jack':
+    case 'Queen':
+    case 'King': this.value = 10; break;
+    default: this.value = rank
+  }
 }
 
 export { Card };
