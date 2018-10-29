78264277f9b79acfcf5c317113fa24f9bd263ed0
Colton Boston <coltonbboston@gmail.com>
2018-10-29 17:44:22 -0500

    add name and minimumTotal parameters and add functions to call Person prototype with specific page elements

--- js/dealer.js
+++ js/dealer.js
@@ -1,7 +1,11 @@
 import { Person } from './person';
+import * as Page from './page';
 
 function Dealer(){
   Person.call(this);
+
+  this.name = 'Dealer';
+  this.minimumTotal = 17;
 }
 
 Dealer.prototype = Object.create(Person.prototype);
@@ -11,5 +15,13 @@ Object.defineProperty(Dealer.prototype, 'constructor', {
     enumerable: false,
     writable: true
 });
+
+Dealer.prototype.renderHand = function(){
+  Person.prototype.renderHand.call(this, Page.displays.dealerHand);
+}
+
+Dealer.prototype.renderHandValue = function(){
+  Person.prototype.renderHandValue.call(this, Page.displays.dealerTotal);
+}
 
 export { Dealer };
