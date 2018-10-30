73eb8c0573284100580392c5a983a1ca07185849
Colton Boston <coltonbboston@gmail.com>
2018-10-30 00:01:34 -0500

    edit updateHandValue function to return the highest value under 21 and remove old functions that did the same; move code to render a hand's cards to js/card.js

--- js/person.js
+++ js/person.js
@@ -2,43 +2,34 @@ function Person(){
   this.hand = {
     cards: [],
     value: 0,
-    numOfHighAces: 0
+    numOfAces: 0
   };
 }
 
 Person.prototype.addToHand = function(card){
   this.hand.cards.push(card);
-  this.updateHandValue();
   if (card.rank === 'Ace'){
-    this.hand.numOfHighAces++;
+    this.hand.numOfAces++;
   }
+  this.updateHandValue();
+
 }
 
 Person.prototype.updateHandValue = function(){
-  this.hand.value = this.hand.cards.reduce((acc, card) => acc + card.value, 0);
-}
-
-Person.prototype.reduceOneHighAce = function(){
-  let index = this.hand.cards.findIndex((card) => card.value === 11);
-  console.log(`Reducing ${this.name}'s ${this.hand.cards[index].rank} of ${this.hand.cards[index].suit} to 1`);
-
-  this.hand.cards[index].value = 1;
-  this.hand.numOfHighAces--;
-  this.updateHandValue();
-  this.renderHand();
+  let value = this.hand.cards.reduce((acc, card) => acc + card.value, 0);
+  if (this.hand.numOfAces > 0 && value > 21){
+    for (let i = 0; i < this.hand.numOfAces; i++){
+      value -= 10;
+      if (value <=21) break;
+    }
+  }
+  this.hand.value = value;
 }
 
 Person.prototype.renderHand = function(parentElement){
   parentElement.innerHTML = '';
   this.hand.cards.forEach(function(card){
-    let cardDiv = document.createElement('div');
-    if (!card.isFaceUp){
-      cardDiv.innerHTML = 'Hidden';
-      console.log(card);
-    } else {
-      cardDiv.innerHTML = `${card.rank} of ${card.suit}`;
-    }
-    parentElement.append(cardDiv);
+    parentElement.appendChild(card.render());
   });
 }
 
@@ -48,27 +39,13 @@ Person.prototype.renderHandValue = function(parentElement){
 }
 
 Person.prototype.checkForBust = function(){
-  if (this.hand.value > 21){
-    if (this.hand.numOfHighAces > 0){
-      this.reduceOneHighAce();
-      this.checkForBust();
-    } else {
-      console.log('Bust');
-      return true;
-    }
-  } else {
-    return false;
-  }
+  return this.hand.value > 21;
 }
 
 Person.prototype.clearHand = function(){
   this.hand.cards = [];
   this.hand.value = 0;
-  this.hand.numOfHighAces = 0;
-}
-
-Person.prototype.findFirstHighAceIndex = function(){
-  let index = this.hand.cards.findIndex((card) => card.value === 11);
+  this.hand.numOfAces = 0;
 }
 
 Person.prototype.checkForBlackjack = function(){
