6e71016066b0195369b7b0f4d349fac8c6251cae
Colton Boston <coltonbboston@gmail.com>
2018-10-30 21:44:45 -0500

    update to work with Hand obj and improve functions for splitting a hand

--- js/player.js
+++ js/player.js
@@ -1,4 +1,5 @@
 import { Person } from './person';
+import { Hand } from './hand';
 import * as Page from './page';
 
 function Player(){
@@ -8,6 +9,8 @@ function Player(){
   this.money = 100;
   this.wager = 0;
   this.hasSplitHand = false;
+  this.splitHand = {};
+  this.hand.setDisplays(Page.displays.playerHand, Page.displays.playerTotal);
 }
 
 Player.prototype = Object.create(Person.prototype);
@@ -20,14 +23,10 @@ Object.defineProperty(Player.prototype, 'constructor', {
 
 // Render all of player's data that changes
 Player.prototype.renderAll = function(){
-  this.renderHand();
+  this.hand.render();
+  this.hand.renderValue();
   this.renderMoney();
   this.renderWager();
-}
-
-Player.prototype.renderHand = function(){
-  Person.prototype.renderHand.call(this, Page.displays.playerHand);
-  Person.prototype.renderHandValue.call(this, Page.displays.playerTotal);
 }
 
 Player.prototype.renderMoney = function(){
@@ -35,19 +34,87 @@ Player.prototype.renderMoney = function(){
 }
 
 Player.prototype.renderWager = function(){
-  Page.displays.wager.innerHTML = `Current wager: $${this.wager}`;
+  if (this.hasSplitHand){
+    Page.displays.wager.innerHTML = `Current wager: $${this.wager * 2} (split)`;
+  } else {
+    Page.displays.wager.innerHTML = `Current wager: $${this.wager}`;
+  }
 }
 
 Player.prototype.clearWager = function(){
   this.wager = 0;
 }
 
-Player.prototype.canSplitHand = function(){
-  if (this.hand.cards.length === 2 && this.hand.cards[0].value === this.hand.cards[1].value){
+Player.prototype.hasActiveHand = function(){
+  if (this.hasSplitHand){
+    return this.hand.isActive || this.splitHand.isActive;
+  } else {
+    return this.hand.isActive;
+  }
+}
+
+Player.prototype.getActiveHand = function(){
+  if (this.hasSplitHand) {
+    return this.hand.isActive ? this.hand : this.splitHand;
+  } else {
+    return this.hand;
+  }
+}
+
+Player.prototype.hasHandToCompare = function(){
+  if (this.hasSplitHand){
+    return !this.hand.isCompleted || !this.splitHand.isCompleted;
+  } else {
+    return !this.hand.isCompleted;
+  }
+}
+
+Player.prototype.getHandToCompare = function(){
+  if (this.hasSplitHand) {
+    return !this.hand.isCompleted ? this.hand : this.splitHand;
+  } else {
+    return this.hand;
+  }
+}
+
+Player.prototype.canSplitPair = function(){
+  if (this.hand.cards.length === 2 &&
+      this.hand.cards[0].value === this.hand.cards[1].value &&
+      this.money >= this.wager &&
+      this.hand.cards[0].rank !== 'Ace'){
     return true;
   } else {
     return false;
   }
+}
+
+Player.prototype.splitPair = function(){
+  this.splitHand = new Hand();
+  this.splitHand.setDisplays(Page.displays.playerSplitHand, Page.displays.playerSplitTotal);
+  this.hasSplitHand = true;
+  this.splitHand.addCard(this.hand.cards.pop());
+  this.hand.updateValue();
+  this.money -= this.wager;
+  this.renderMoney();
+  this.renderWager();
+
+  this.hand.render();
+  this.hand.renderValue();
+  this.splitHand.render();
+  this.splitHand.renderValue();
+}
+
+Player.prototype.renderSplitHand = function(){
+  let display = this.splitHand.display;
+  display.innerHTML = '';
+  this.splitHand.cards.forEach(function(card){
+    display.appendChild(card.render());
+  });
+}
+
+Player.prototype.renderSplitHandValue = function(){
+  let value = this.splitHand.value;
+  this.splitHand.totalDisplay.innerHTML = value;
 }
 
 export { Player };
